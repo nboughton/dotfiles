7d9be0f7a4a9aabf72e9032a36bc93581a5febe2
Nick Boughton <nicholasboughton@gmail.com>
2020-11-25 01:07:22 +0000

    keybinding for floating ranger window

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -128,9 +128,9 @@ handle_image() {
     local mimetype="${1}"
     case "${mimetype}" in
         ## SVG
-        # image/svg+xml|image/svg)
-        #     convert -- "${FILE_PATH}" "${IMAGE_CACHE_PATH}" && exit 6
-        #     exit 1;;
+        image/svg+xml|image/svg)
+            convert -- "${FILE_PATH}" "${IMAGE_CACHE_PATH}" && exit 6
+            exit 1;;
 
         ## DjVu
         # image/vnd.djvu)
@@ -154,20 +154,20 @@ handle_image() {
             exit 7;;
 
         ## Video
-        # video/*)
-        #     # Thumbnail
-        #     ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
-        #     exit 1;;
+        #  video/*)
+        #    # Thumbnail
+        #    ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
+        #    exit 1;;
 
         ## PDF
         # application/pdf)
-        #     pdftoppm -f 1 -l 1 \
-        #              -scale-to-x "${DEFAULT_SIZE%x*}" \
-        #              -scale-to-y -1 \
-        #              -singlefile \
-        #              -jpeg -tiffcompression jpeg \
-        #              -- "${FILE_PATH}" "${IMAGE_CACHE_PATH%.*}" \
-        #         && exit 6 || exit 1;;
+        #    pdftoppm -f 1 -l 1 \
+        #             -scale-to-x "${DEFAULT_SIZE%x*}" \
+        #             -scale-to-y -1 \
+        #             -singlefile \
+        #             -jpeg -tiffcompression jpeg \
+        #             -- "${FILE_PATH}" "${IMAGE_CACHE_PATH%.*}" \
+        #        && exit 6 || exit 1;;
 
 
         ## ePub, MOBI, FB2 (using Calibre)
