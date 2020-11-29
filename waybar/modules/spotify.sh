7ce98d3bc1336cefeee08a3603cb3a3ce329f450
Nick Boughton <nicholasboughton@gmail.com>
2020-11-29 02:47:19 +0000

    fix string limit for spotify widget

--- waybar/modules/spotify.sh
+++ waybar/modules/spotify.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 class=$(playerctl metadata --player=spotify --format '{{lc(status)}}')
 icon=""
+limit=50
 
 if [[ $class == "playing" ]]; then
 	info=$(playerctl metadata --player=spotify --format '{{artist}} {{album}} - {{title}}' | sed 's/"//g')
-	if [[ ${#info} > 50 ]]; then
-		info=$(echo $info | cut -c1-50)"..."
+	if [[ ${#info} -gt $limit ]]; then
+		info=$(echo $info | cut -c1-$limit)"..."
 	fi
 	text=$icon" "$info
 elif [[ $class == "paused" ]]; then
