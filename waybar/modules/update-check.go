1926c30b6e8ed84e2c6ba77ca5ebd1d25ddb3baa
Nick Boughton <nicholasboughton@gmail.com>
2020-12-08 22:10:18 +0000

    better error checking on pacman module

--- waybar/modules/update-check.go
+++ waybar/modules/update-check.go
@@ -20,13 +20,15 @@ func main() {
 	o := jsonOutput{}
 
 	pac, err := exec.Command("checkupdates").CombinedOutput()
-	if err != nil {
+	if err.Error() == "exit status 1" {
 		o.Tooltip = err.Error()
+		o.Class = "error"
 	}
 
 	aur, err := exec.Command("yay", "--devel", "-Qu").CombinedOutput()
 	if err != nil {
 		o.Tooltip += err.Error()
+		o.Class = "error"
 	}
 
 	updates := append(strings.Split(string(pac), "\n"), strings.Split(string(aur), "\n")...)
@@ -34,15 +36,21 @@ func main() {
 
 	n := len(updates)
 
-	o.Class = "no-updates"
-	if n > 0 {
-		o.Class = "updates"
-		o.Tooltip = strings.Join(updates, "\n")
-	}
+	if o.Class == "error" {
+		o.Text = "!"
+		o.Alt = "!"
+		o.Percentage = 0
+	} else {
+		o.Class = "no-updates"
+		if n > 0 {
+			o.Class = "updates"
+			o.Tooltip = strings.Join(updates, "\n")
+		}
 
-	o.Text = fmt.Sprintf("%d", n)
-	o.Alt = fmt.Sprintf("%d", n)
-	o.Percentage = n
+		o.Text = fmt.Sprintf("%d", n)
+		o.Alt = fmt.Sprintf("%d", n)
+		o.Percentage = n
+	}
 
 	json.NewEncoder(os.Stdout).Encode(o)
 }
