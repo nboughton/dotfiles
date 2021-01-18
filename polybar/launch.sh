d70f68dd012484ede917b28e6b8b5e5c740a57b5
Nick Boughton <nicholasboughton@gmail.com>
2021-01-18 14:17:28 +0000

    added i3 stuff for X session

---
+++ polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
+
+echo "Polybar launched..."
